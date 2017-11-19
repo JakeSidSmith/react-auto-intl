e177b4cce5a2490e5f67b61a9defe750f1cd3e86
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-19 21:04:07 +0000

    Compile ts to js

--- src/cli/parser/index.ts
+++ src/cli/parser/index.ts
@@ -1,20 +1,38 @@
-import { readFile } from 'fs';
 import * as glob from 'glob';
 import { Tree } from 'jargs';
 import { some } from 'lodash';
 import * as ts from 'typescript';
 
 let tsTarget: ts.ScriptTarget;
+let tsModule: ts.ModuleKind;
 
 const MATCH_TS_EXTENSION = /\.tsx?$/i;
-const MATCHES_ES = /^es/i;
 
 const CWD = process.cwd();
-const UTF8 = 'utf8';
 const DEFAULT_PATTERN = '**/*.@(js|jsx|ts|tsx)';
 
 const GLOB_OPTIONS = {
   absolute: true,
+};
+
+const MODULE_MAPPING: {[i: string]: keyof typeof ts.ModuleKind} = {
+  none: 'None',
+  commonjs: 'CommonJS',
+  amd: 'AMD',
+  umd: 'UMD',
+  system: 'System',
+  es2015: 'ES2015',
+  esnext: 'ESNext',
+};
+
+const SCRIPT_MAPPING: {[i: string]: keyof typeof ts.ScriptTarget} = {
+  es3: 'ES3',
+  es5: 'ES5',
+  es2015: 'ES2015',
+  es2016: 'ES2016',
+  es2017: 'ES2017',
+  esnext: 'ESNext',
+  latest: 'Latest',
 };
 
 interface Sources {[i: string]: string}
@@ -30,24 +48,38 @@ const getFilePaths = (pattern: string, callback: GetFilePathsCallback) => {
 const getSources = (paths: string[], callback: GetSourcesCallback) => {
   const sources: Sources = {};
 
-  paths.map((path) => readFile(path, UTF8, (error: NodeJS.ErrnoException, source: string) => {
-    sources[path] = source;
+  paths.map((path) => {
+    const source = ts.sys.readFile(path);
+
+    if (source) {
+      if (MATCH_TS_EXTENSION.test(path)) {
+        sources[path] = ts.transpileModule(
+          source,
+          {
+            compilerOptions: {
+              module: tsModule,
+              target: tsTarget,
+            },
+          }
+        ).outputText;
+      } else {
+        sources[path] = source;
+      }
+    }
 
     if (Object.keys(sources).length === paths.length) {
       callback(sources);
     }
-  }));
+
+  });
 };
 
 const traverseSources = (sources: Sources) => {
-  for (const path in sources) {
-    if (sources.hasOwnProperty(path)) {
-      const source = sources[path];
-
-      // TODO: Continue from here
-      ts.createSourceFile(path, source, tsTarget);
-    }
-  }
+  // for (const path in sources) {
+  //   if (sources.hasOwnProperty(path)) {
+  //     const source = sources[path];
+  //   }
+  // }
 };
 
 // const traverse = () => {
@@ -57,20 +89,36 @@ const traverseSources = (sources: Sources) => {
 const findTsTarget = () => {
   const tsConfigLocation = ts.findConfigFile(CWD, ts.sys.fileExists);
 
-  const { config: { compilerOptions: { target = ts.ScriptTarget.Latest } }, error } = tsConfigLocation ?
+  const {
+    config: {
+      compilerOptions: {
+        target: configTarget = ts.ScriptTarget.Latest,
+        module: configModule = ts.ModuleKind.CommonJS,
+      },
+    },
+    error,
+  } = tsConfigLocation ?
     ts.readConfigFile(tsConfigLocation, ts.sys.readFile) : ({} as any);
 
-  if (typeof target === 'string' && !(target.replace(MATCHES_ES, 'ES') in ts.ScriptTarget)) {
-    console.error(`Invalid target in tsconfig: ${target}`); // tslint:disable-line:no-console
+  if (typeof configTarget === 'string' && !(configTarget.toLowerCase() in SCRIPT_MAPPING)) {
+    console.error(`Invalid target in tsconfig: ${configTarget}`); // tslint:disable-line:no-console
+  }
+
+  if (typeof configModule === 'string' && !(configModule.toLowerCase() in MODULE_MAPPING)) {
+    console.error(`Invalid module in tsconfig: ${configModule}`); // tslint:disable-line:no-console
   }
 
   if (error) {
     console.error(error); // tslint:disable-line:no-console
     process.exit(1);
   } else {
-    tsTarget = typeof target === 'string' &&
-      ts.ScriptTarget[target.replace(MATCHES_ES, 'ES') as keyof typeof ts.ScriptTarget] ||
+    tsTarget = typeof configTarget === 'string' &&
+      ts.ScriptTarget[SCRIPT_MAPPING[configTarget.toLowerCase()]] ||
       ts.ScriptTarget.Latest;
+
+    tsModule = typeof configModule === 'string' &&
+      ts.ModuleKind[MODULE_MAPPING[configModule.toLowerCase()]] ||
+      ts.ModuleKind.CommonJS;
   }
 };
 
@@ -87,6 +135,7 @@ const parse = (tree: Tree) => {
     if (some(paths, MATCH_TS_EXTENSION)) {
       findTsTarget();
     }
+
     getSources(paths, traverseSources);
   });
 };
