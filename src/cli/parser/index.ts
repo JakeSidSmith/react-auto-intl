e08164dd9ffa49700dcef9b9b7752af377b1c476
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-19 19:35:38 +0000

    Load tsconfig target

--- src/cli/parser/index.ts
+++ src/cli/parser/index.ts
@@ -2,27 +2,24 @@ import { readFile } from 'fs';
 import * as glob from 'glob';
 import { Tree } from 'jargs';
 import * as ts from 'typescript';
+import { log } from 'util';
+
+let tsTarget: number;
+
+const MATCHES_ES = /^es/i;
 
 const CWD = process.cwd();
-const TS_CONFIG = ts.findConfigFile(CWD, ts.sys.fileExists);
 const UTF8 = 'utf8';
 const DEFAULT_PATTERN = '**/*.@(js|jsx|ts|tsx)';
+
 const GLOB_OPTIONS = {
   absolute: true,
 };
 
-const { config: tsconfig, error: tsconfigError } = TS_CONFIG ? ts.readConfigFile(TS_CONFIG, ts.sys.readFile) : {
-  config: {
-    compilerOptions: {
-      target: ts.ScriptTarget.Latest,
-    },
-  },
-  error: undefined,
-};
-
-if (tsconfigError) {
-  console.error(tsconfigError);
-  process.exit(1);
+interface TSConfig {
+  compilerOptions: {
+    target: string;
+  };
 }
 
 interface Sources {[i: string]: string}
@@ -53,7 +50,7 @@ const traverseSources = (sources: Sources) => {
       const source = sources[path];
 
       // TODO: Continue from here
-      ts.createSourceFile(path, source, tsconfig.compilerOptions.target);
+      ts.createSourceFile(path, source, tsTarget);
     }
   }
 };
@@ -65,7 +62,26 @@ const traverse = () => {
 const parse = (tree: Tree) => {
   const { pattern = DEFAULT_PATTERN } = tree.args;
 
-  getFilePaths(pattern, (paths) => getSources(paths, traverseSources));
+  getFilePaths(pattern, (paths) => {
+    const anyFilesAreTS = true;
+
+    if (anyFilesAreTS) {
+      const tsconfigLocation = ts.findConfigFile(CWD, ts.sys.fileExists);
+
+      const { config: { compilerOptions: { target = ts.ScriptTarget.Latest } }, error } = tsconfigLocation ?
+        ts.readConfigFile(tsconfigLocation, ts.sys.readFile) : ({} as any);
+
+      if (error) {
+        console.error(error);
+        process.exit(1);
+      } else {
+        tsTarget = typeof target === 'string' ?
+          ts.ScriptTarget[target.replace(MATCHES_ES, 'ES') as keyof typeof ts.ScriptTarget] :
+          ts.ScriptTarget.Latest;
+      }
+    }
+    getSources(paths, traverseSources);
+  });
 };
 
 export default parse;
