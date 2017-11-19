d6f4af7d7ca9cfde2992c5468bcfeb13ae2d80e2
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-19 18:41:59 +0000

    Collect absolute paths

--- src/cli/parser/index.ts
+++ src/cli/parser/index.ts
@@ -2,11 +2,14 @@ import * as glob from 'glob';
 import { Tree } from 'jargs';
 
 const DEFAULT_PATTERN = '**/*.@(js|jsx|ts|tsx)';
+const GLOB_OPTIONS = {
+  absolute: true,
+};
 
 const parse = (tree: Tree) => {
   const { pattern = DEFAULT_PATTERN } = tree.args;
 
-  glob(pattern, (error: Error | null, files: string[]) => {
+  glob(pattern, GLOB_OPTIONS, (error: Error | null, files: string[]) => {
     console.log(files);
   });
 };
